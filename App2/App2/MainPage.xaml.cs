19859a76bd7efa29c938106c3a4b9ec45ecb94a9
Eriks Berzins <eeriksb@gmail.com>
2016-08-24 17:48:23 +0300

    Initial

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class NavigationPage : ContentPage
+    {
+        private Menu menu;
+
+        public NavigationPage()
+        {
+            InitializeComponent();
+        }
+
+        public NavigationPage(Menu menu)
+        {
+            this.menu = menu;
+        }
+
+        async void login (object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Menu());
+        }
+    }
+}
---
+++ App2/App2/Menu.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class Menu : ContentPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+        async void add_place(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new InformationPage());
+        }
+    }
+}
